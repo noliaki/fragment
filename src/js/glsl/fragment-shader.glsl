fe6e2600c66ce28d2f088c72bcef8232301f21c9
noliaki <noliaki@gmail.com>
2019-10-23 17:09:50 +0900

    [wip]

--- src/js/glsl/fragment-shader.glsl
+++ src/js/glsl/fragment-shader.glsl
@@ -9,15 +9,17 @@ const float maxDuration = 1.0 - maxDelay;
 
 void main() {
   float delay = (distance(vUv.x, 0.5) / 0.5 + distance(vUv.y, 0.5) / 0.5) / 2.0;
-  float progressTime = (sin(uTime / 70.0) + 1.0) / 2.0;
+  float progressTime = (sin(uTime / 30.0) + 1.0) / 2.0;
   float progress = clamp(progressTime - delay * maxDelay, 0.0, maxDuration) / maxDuration;
+  float peek = abs(step(0.5, progress) - progress) / 0.5 + 1.0;
 
-  float noise = snoise(vec3(vUv.x, vUv.y, uTime / 100.0));
-  vec4 mapColor = texture2D(uTextureMap, vUv + (noise / 2.0));
+  float noise = (snoise(vec3(vUv.x, vUv.y, uTime / 100.0)) + 1.0) / 2.0;
+  // vec4 mapColor = texture2D(uTextureMap, vUv + (noise / (2.0 * peek + 1.0)));
+  vec4 mapColor = texture2D(uTextureMap, vUv * (noise * peek));
   float colorAvg = (((mapColor.r + mapColor.g + mapColor.b) / 3.0) * 2.0) - 1.0;
-  vec4 beforeColor = texture2D(uTexture1, vUv + progress * colorAvg);
-  vec4 afterColor = texture2D(uTexture2, vUv + (1.0 - progress) * colorAvg);
+  vec4 beforeColor = texture2D(uTexture1, vUv + progressTime * colorAvg * delay * 5.0);
+  vec4 afterColor = texture2D(uTexture2, vUv + (1.0 - progressTime) * colorAvg * delay * 5.0);
 
-  gl_FragColor = mix(beforeColor, afterColor, progress);
+  gl_FragColor = mix(beforeColor, afterColor, progressTime);
   // gl_FragColor = vec4(vec3((noise + 1.0) / 2.0), 1.0);
 }
--- src/js/glsl/vertex-shader.glsl
+++ src/js/glsl/vertex-shader.glsl
@@ -6,6 +6,9 @@ const float PI = 3.141592653589793238462643383279502884197169399375105820;
 const float PI2 = 2.0 * PI;
 
 void main(){
-  vUv = uv;
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  vUv = position.xy + 0.5;
+  vec4 transform = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  // transform.x *= sin(uTime / 200.0) * 2.0;
+  // transform.y *= cos(uTime / 250.0) * 1.0;
+  gl_Position = transform;
 }
